7399b82fdc1a617b9187feda5d7be69d79db5d40
RAMIN <raminabbasi2121@gmail.com>
2021-12-09 13:02:49 +0330

    use async and await for Parallel and Sequential background task

--- app/src/main/java/com/example/coroutine_and_flow/ui/MainViewModel.kt
+++ app/src/main/java/com/example/coroutine_and_flow/ui/MainViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.ViewModel
 import com.example.coroutine_and_flow.datasource.network.CountryModel
 import com.example.coroutine_and_flow.datasource.network.MainApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
 import javax.inject.Inject
+import kotlin.system.measureTimeMillis
 
 @HiltViewModel
 class MainViewModel
@@ -24,11 +24,45 @@ constructor(
     }
 
     private fun countryList() {
-        CoroutineScope(Dispatchers.IO).launch {
-            service.getCountries()
-                .let {
+        //Parallel background task with async and await
+        CoroutineScope(IO).launch {
+            val time = measureTimeMillis {
+                val result1 = async {
+                    service.getCountries()
+                }
+
+                val result2 = async {
+                    getNumber1()
+                }
+                result1.await().let {
                     countries.postValue(it)
                 }
+                println("debug: getNumber1 is: ${result2.await()} ")
+
+            }
+            println("debug: job1 and job2 are complete. It took $time ms")
+
+            //Sequential background task with async and await
+            /*val time = measureTimeMillis {
+                val result1 = async {
+                    getNumber1()
+                }.await()
+
+                val result2 = async {
+                    getNumber2()
+                }.await()
+            }
+            println("debug: job1 and job2 are complete. It took $time ms")*/
         }
     }
+
+    private suspend fun getNumber1(): Int {
+        delay(2000)
+        return 21
+    }
+
+    /*private suspend fun getNumber2(): Int {
+        delay(2000)
+        return 13
+    }*/
 }
