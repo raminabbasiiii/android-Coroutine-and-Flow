b63ac040c7b983c1f4bf1ff2e9966d9b39c6d393
RAMIN <raminabbasi2121@gmail.com>
2022-01-02 12:58:33 +0330

    change viewModel

--- app/src/main/java/com/example/coroutine_and_flow/ui/MainViewModel.kt
+++ app/src/main/java/com/example/coroutine_and_flow/ui/MainViewModel.kt
@@ -1,18 +1,12 @@
 package com.example.coroutine_and_flow.ui
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.coroutine_and_flow.datasource.network.CountryModel
 import com.example.coroutine_and_flow.datasource.network.MainApiService
-import com.example.coroutine_and_flow.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,29 +14,21 @@ import javax.inject.Inject
 class MainViewModel
 @Inject
 constructor(
-    private val repository: Repository
+    private val service: MainApiService
 ): ViewModel() {
 
-    private val _countries : MutableLiveData<List<CountryModel>> = MutableLiveData()
-
-    val countries: LiveData<List<CountryModel>>
-    get() = _countries
+    val countries = MutableLiveData<List<CountryModel>>()
 
     init {
         countryList()
     }
 
     private fun countryList() {
-        repository
-            .getCountries()
-            .onEach { _countries.value = it }
-            .launchIn(viewModelScope)
-       /*viewModelScope.launch {
-           repository
-               .getCountries()
-               .collect {
-                   _countries.value = it
-               }
-       }*/
+        CoroutineScope(Dispatchers.IO).launch {
+            service.getCountries()
+                .let {
+                    countries.postValue(it)
+                }
+        }
     }
 }
